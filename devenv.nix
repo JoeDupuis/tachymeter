c96557e8958665cdf7f82e372d1961a78dfa5443
Joé Dupuis <joe@dupuis.io>
2025-04-27 19:17:00 -0700

    Devenv

---
+++ devenv.nix
@@ -0,0 +1,23 @@
+{ config, pkgs, ... }:
+
+with pkgs; {
+  env = {
+    LD_LIBRARY_PATH = "${config.devenv.profile}/lib";
+  };
+
+  packages = [
+    git
+    libyaml
+    sqlite-interactive
+    sqlite
+    openssl
+    postgresql_17
+    mariadb_114
+
+    cargo
+    rustc
+  ];
+
+  languages.ruby.enable = true;
+  languages.ruby.versionFile = ./.ruby-version;
+}
