305c88da871f5cf516f58ff0999dfbfdf179bd10
Joé Dupuis <joe@dupuis.io>
2025-04-27 19:24:04 -0700

    Tachymeter::CLI

---
+++ lib/tachymeter/cli.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'tachymeter'
+
+module Tachymeter
+  class CLI
+    def initialize
+      @scenario = Tachymeter::Scenario.new
+      Tachymeter::Application.new
+    end
+
+    def run
+      results = 5.times.map do
+        Tachymeter::Runner.new(timeout: 5, runs: Tachymeter::Runner::CPU_COUNT).start { scenario.run }
+      end
+
+      results.map(&:sole).sort_by!(&:average_frequency)
+      max, min = results.last.sole.total_frequency, results.first.sole.total_frequency
+      delta = (max - min)
+
+      pp results.third.sole.total_frequency, delta
+    end
+
+    private
+
+    attr_reader :scenario
+  end
+end
