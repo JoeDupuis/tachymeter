c32d6c5fc34b51be75d9f503401ca5e2b14130b1
Joé Dupuis <joe@dupuis.io>
2025-04-28 21:45:13 -0700

    Refactor fixtures to match rails fixtures

--- test/support/fixture_helper.rb
+++ test/support/fixture_helper.rb
@@ -1,31 +1,29 @@
+# frozen_string_literal: true
+
 require "json"
 require "fileutils"
+require "tachymeter"
 
 module FixtureHelper
-  def self.load_or_build(name = :run, **runner_opts, &block)
-    path = fixture_path(name)
+  def runs(name = :run, **runner_opts, &block)
+    fixture_dir = File.expand_path("../fixtures/runs", __dir__)
+    fixture_file = File.join(fixture_dir, "#{name}.json")
 
-    return cached_fixture(path) if File.exist?(path) && !ENV["REGEN_FIXTURES"]
-
-    if block_given?
-      data = Tachymeter::Runner.new(**runner_opts).start(&block)
-    else
-      scenario = Scenario.new
-      data = Tachymeter::Runner.new(**runner_opts).start { scenario.run }
+    if File.exist?(fixture_file) && !ENV["REGEN_FIXTURE"]
+      return JSON.parse(File.read(fixture_file), symbolize_names: true)
+                 .map { |h| Tachymeter::ResultSet.from_h(h) }
     end
 
-    FileUtils.mkdir_p(File.dirname(path))
-    File.write(path, JSON.pretty_generate(data.map(&:to_h)))
+    data = if block_given?
+      Tachymeter::Runner.new(**runner_opts).start(&block)
+    else
+      scenario = Tachymeter::Scenario.new
+      Tachymeter::Runner.new(**runner_opts).start { scenario.run }
+    end
+
+    FileUtils.mkdir_p(fixture_dir)
+    File.write(fixture_file, JSON.pretty_generate(data.map(&:to_h)))
 
     data
-  end
-
-  def self.fixture_path(name)
-    File.expand_path("../fixtures/#{name.to_s}.json", __dir__)
-  end
-
-  def self.cached_fixture(path)
-    JSON.parse(File.read(path), symbolize_names: true)
-      .map { |h| Tachymeter::ResultSet.from_h(h) }
   end
 end
